413c17a1d8521911570611fbcd5eae070964aa72
NicolasP19 <nicolas.pfaff@hs-furtwangen.de>
2020-03-11 14:36:10 +0100

    Bearbeiten der Klasse Analysebericht Version 0.0.3

--- Analysebericht.java
+++ Analysebericht.java
@@ -2,13 +2,13 @@
  * Beschreiben Sie hier die Klasse Analysebericht.
  * 
  * @author Nicolas Pfaff 
- * @version 0.0.1
+ * @version 0.0.3
  */
 import java.util.*;
 
 public class Analysebericht
 {
-    String Laborantenkürzel;
+    String Laborantenkuerzel;
     String Analysedatum;
     String Laborname;
     String AnalyseObjekt;
@@ -20,29 +20,29 @@ public class Analysebericht
      * Dabei werden die Daten Laborant, Labor, Analyseobjekt, Analysemethode und Analyseergebnis durch Parameter übergeben.
      * Das aktuelle Datum wird durch die Klasse Calendar erzeugt.
      * 
-     * @param Laborantenkürzel
+     * @param Laborantenkuerzel
      * @param Laborname
      * @param AnalyseObjekt
      * @param Analysemethode
      * @param Analyseergebnis
      */
-    public Analysebericht(String Laborant, String Labor, String Analyseobjekt, String Analysemethode, String Analyseergebnis)
+    public Analysebericht(String Laborantenkuerzel, String Laborname, String AnalyseObjekt, String Analysemethode, String Analyseergebnis)
     {
-        Laborantenkürzel = Laborant;
+        this.Laborantenkuerzel = Laborantenkuerzel;
         Calendar date = Calendar.getInstance();
         Analysedatum = date.get(Calendar.DAY_OF_MONTH ) + "." + (date.get(Calendar.MONTH) + 1 ) + "." + date.get(Calendar.YEAR);
-        this.Laborname = Labor;
-        this.AnalyseObjekt = Analyseobjekt;
+        this.Laborname = Laborname;
+        this.AnalyseObjekt = AnalyseObjekt;
         this.Analysemethode = Analysemethode;
         this.Analyseergebnis = Analyseergebnis;
     }
 
-    public void Analyseberichtbearbeiten(String Laborant, String Erstellungsdatum, String Labor, String Analyseobjekt, String Analysemethode, String Analyseergebnis)
+    public void Analyseberichtbearbeiten(String Laborantenkuerzel, String Erstellungsdatum, String Laborname, String AnalyseObjekt, String Analysemethode, String Analyseergebnis)
     {
-        this.Laborantenkürzel = Laborant;
+        this.Laborantenkuerzel = Laborantenkuerzel;
         this.Analysedatum = Erstellungsdatum;
-        this.Laborname = Labor;
-        this.AnalyseObjekt = Analyseobjekt;
+        this.Laborname = Laborname;
+        this.AnalyseObjekt = AnalyseObjekt;
         this.Analysemethode = Analysemethode;
         this.Analyseergebnis = Analyseergebnis;
     }
