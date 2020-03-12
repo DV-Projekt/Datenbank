065090cb717f3c0c4d1af6caf71eb8dbeb1159f7
ObberDeShooter <lennartburkart@online.de>
2020-03-12 18:07:40 +0100

    die methoden: bericht erstellen und notfallkontakt wurden überarbeitet

--- Patientenakte.java
+++ Patientenakte.java
@@ -5,6 +5,7 @@
  * @author (Lennart Burkart, Ricarda Henkel)
  * @version (0.0.2)
  */
+import java.util.*;
 public class Patientenakte
 {
     private String Name;
@@ -17,6 +18,8 @@ public class Patientenakte
     private int Telefonnummer;
     private String Vorerkrankungen;
     private String Allergien;
+    private ArrayList<Analysebericht> Analyseberichte;
+    private ArrayList<Notfallkontakt> Notfallkontakte;
     
     //erstellt eine neue Patientenakte und weist ihr die eingegebenen Werte zu.
     public Patientenakte(String N, int Alt, String Ad, String Gesch, int KrankNr, 
@@ -32,6 +35,8 @@ public class Patientenakte
         Telefonnummer=Tel;
         Vorerkrankungen=Vor;
         Allergien=All;
+        Analyseberichte = new ArrayList<Analysebericht>();
+        Notfallkontakte = new ArrayList<Notfallkontakt>();
     }
     
     //ändert die Werte einer bereits vorhandenen Patientenakte auf die neu 
@@ -49,10 +54,15 @@ public class Patientenakte
         Allergien=All;  
     }
     
-    //erstellt einen neuen Analysebericht mit den eingegebenen Werten.
-    public void Analyseberichtanlegen()
+    //erstellt einen neuen Analysebericht mit den eingegebenen Werten und fügt
+    //ihn der Liste von Analyseberichten hinzu.
+    public void Analyseberichtanlegen(String Laborantenkuerzel, String 
+    Laborname, String AnalyseObjekt, String Analysemethode, 
+    String Analyseergebnis)
     {
-        
+        Analysebericht Bericht=new Analysebericht(Laborantenkuerzel, Laborname,
+        AnalyseObjekt, Analysemethode, Analyseergebnis);
+        Analyseberichte.add(Bericht);
     }
     public void Analyseberichtsuchen()
     {
@@ -70,9 +80,13 @@ public class Patientenakte
     {
         
     }
+    
+    //Erstellt einen neuen Notfallkontakt mit den eingegebenen Werten und fügt
+    //ihn der Liste mit Notfallkontakten hinzu.
     public void Notfallkontakterstellen()
     {
-        
+        Notfallkontakt Kontakt = new Notfallkontakt();
+        Notfallkontakte.add(Kontakt);
     }
     public void Notfallkontaktlöschen()
     {
