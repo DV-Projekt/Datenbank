4b16a84fed8c13d0609fc15e54bed43e430c2a6c
NicolasP19 <nicolas.pfaff@hs-furtwangen.de>
2020-03-12 16:12:37 +0100

    Erstellen der Exportierenmethode und einbinden von Apache POI Version 0.0.4

--- Analysebericht.java
+++ Analysebericht.java
@@ -2,9 +2,19 @@
  * Beschreiben Sie hier die Klasse Analysebericht.
  * 
  * @author Nicolas Pfaff 
- * @version 0.0.3
+ * @version 0.0.4
  */
+
 import java.util.*;
+import org.apache.poi.*;
+import org.apache.poi.xssf.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.commons.collections4.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class Analysebericht
 {
@@ -46,4 +56,58 @@ public class Analysebericht
         this.Analysemethode = Analysemethode;
         this.Analyseergebnis = Analyseergebnis;
     }
+
+    public void Berichtexportieren(String Filename)
+    {
+        String filename = "D:/HFU/2.Semester/DV-Projekt/"+Filename+".xlsx";
+         
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Analysebericht");
+
+        Object[][] werte = new Object [][]{{"Laborantenkuerzel", "Analysedatum","Laborname", "AnalyseObjekt", "Analysemethode", "Analyseergebnis"},
+                {Laborantenkuerzel, Analysedatum, Laborname, AnalyseObjekt,Analysemethode, Analyseergebnis}};
+
+        int rowNum =0;
+        System.out.println("Dokument wird erstellt");
+
+        for (Object[] datatype : werte) {
+            Row row = sheet.createRow(rowNum++);
+            int colNum = 0;
+            for (Object field : werte) {
+                Cell cell = row.createCell(colNum++);
+                if (field instanceof String) {
+                    cell.setCellValue((String) field);
+                } else if (field instanceof Integer) {
+                    cell.setCellValue((Integer) field);
+                }
+            }
+        }
+        
+        try {
+            FileOutputStream outputStream = new FileOutputStream(filename);
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Bericht wurde exportiert");
+        //return name;
+    }
 }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
