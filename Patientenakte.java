72b36e377ce231db1160225ad398621b0ab5dfaf
Ricarda <ricardahenkel@hotmail.de>
2020-04-08 20:32:53 +0200

    Get Methoden eingefügt

--- Patientenakte.java
+++ Patientenakte.java
@@ -145,11 +145,55 @@ public class Patientenakte
         return KrankenkassenNr;
     }
     
+    //Get Methode für Name
     public String getName ()
     {
         return Name;
     }
     
+    //Get Methode für Alter
+    public String getAlter ()
+    {
+        return Alter;
+    }
+    
+    //Get Methode für Geschlecht
+    public String getGeschlecht ()
+    {
+        return Geschlecht;
+    }
+    
+    //Get Methode für Blutgruppe
+    public String getBlutgruppe ()
+    {
+        return Blutgruppe;
+    }
+    
+     //Get Methode für ZuständigerArzt
+    public String getZuständigerArzt ()
+    {
+        return ZuständigerArzt;
+    }
+    
+     //Get Methode für Telefonnummer
+    public String getTelefonnummer ()
+    {
+        return Telefonnummer;
+    }
+    
+     //Get Methode für Vorerkrankungen
+    public String getVorerkrankungen ()
+    {
+        return Vorerkrankungen;
+    }
+    
+     //Get Methode für Allergien
+    public String getAllergien ()
+    {
+        return Allergien;
+    }
+    
+
     //ändert die Werte einer bereits vorhandenen Patientenakte auf die neu 
     //eingegebenen Werte
     public void Aktebearbeiten(String Ad, String Gesch, String KrankNr, 
