ed0c779a5bcbb10e0c6d1a8a24ad5a029b8fb6b1
ObberDeShooter <lennartburkart@online.de>
2020-05-11 17:05:26 +0200

    test isLetter

--- MainWindowController.java
+++ MainWindowController.java
@@ -3,7 +3,7 @@
  * Beschreiben Sie hier die Klasse MainWindowController.
  * 
  * @author Nicolas Pfaff, Lennart Burkart
- * @version 0.0.38
+ * @version 0.0.39
  */
 import javafx.application.*;
 import javafx.stage.*;
@@ -223,6 +223,18 @@ public class MainWindowController extends Verwalter
         this.main = main;
     }
 
+    public static boolean isLetter(String s)
+    {
+        for(int i=0;i<s.length();i++)
+        {
+            char c = s.charAt(i);
+            if(Character.isLetter(c)||c==' ')
+                continue;
+        return false;   
+        }
+        return true;
+    }
+    
     @FXML
     public void Notfallkontakterstellen()
     {
